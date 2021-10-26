68d0a4837352db3468bab9ed1b67c129a841cacf
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-10-26 22:10:00 +0300

    add getrandomarray function

--- SelectionSort/src/com/gmail/dev/surovtsev/yaroslav/Main.java
+++ SelectionSort/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -1,8 +1,22 @@
 package com.gmail.dev.surovtsev.yaroslav;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] a = getRandomArray();
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static int[] getRandomArray() {
+        Random rn = new Random();
+        int n = rn.nextInt(20);
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = rn.nextInt(10);
+        }
+        return a;
     }
 }
