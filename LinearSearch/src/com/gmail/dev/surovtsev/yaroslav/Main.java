5e231e6b4a889fa2edb7b72549a45fa432d60d03
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-10-29 21:57:51 +0300

    add implementation method linear search

--- LinearSearch/src/com/gmail/dev/surovtsev/yaroslav/Main.java
+++ LinearSearch/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -13,6 +13,11 @@ public class Main {
     }
 
     public static int linearSearch(int[] array, int element) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == element) {
+                return i;
+            }
+        }
         return -1;
     }
 }
