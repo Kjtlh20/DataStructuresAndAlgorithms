e241c44269711cda627f35a7304eaf6f125fbb90
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-10-29 21:53:58 +0300

    add method linear search without implementation

--- LinearSearch/src/com/gmail/dev/surovtsev/yaroslav/Main.java
+++ LinearSearch/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -1,8 +1,18 @@
 package com.gmail.dev.surovtsev.yaroslav;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] array = new int[]{0, 1, 9, 2, 8, 3, 7, 4, 6, 5};
+        int element = 4;
+        System.out.println(Arrays.toString(array));
+        int index = linearSearch(array, element);
+        System.out.println("element " + element + " at index " + index);
+    }
+
+    public static int linearSearch(int[] array, int element) {
+        return -1;
     }
 }
