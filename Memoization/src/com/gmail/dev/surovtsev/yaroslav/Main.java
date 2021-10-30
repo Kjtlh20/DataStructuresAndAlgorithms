46d0eaacadf008e9bb8aee6423b6ddf58f2ab5d2
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-10-30 22:19:10 +0300

    add test text

--- Memoization/src/com/gmail/dev/surovtsev/yaroslav/Main.java
+++ Memoization/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -16,6 +16,7 @@ public class Main {
             System.out.println("" + n + "! = " + fact.toString());
             System.out.println(System.currentTimeMillis() - start + " ms");
             System.out.println("test merge commit2");
+            System.out.println("test merge commit3");
         }
     }
 
