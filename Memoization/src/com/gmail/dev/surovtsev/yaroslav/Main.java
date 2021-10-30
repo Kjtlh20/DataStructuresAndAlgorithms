9e21e92162a58da17c1ac6c9c97b4f4bd4672693
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-10-30 21:57:10 +0300

    init commit

---
+++ Memoization/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -0,0 +1,20 @@
+package com.gmail.dev.surovtsev.yaroslav;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static Map<Integer, BigInteger> map = new HashMap<>();
+
+    public static void main(String[] args) {
+        Integer n = 10;
+        BigInteger fact = factorial(n);
+        System.out.println("" + n + "! = " + fact);
+    }
+
+    public static BigInteger factorial(Integer number) {
+        return null;
+    }
+}
